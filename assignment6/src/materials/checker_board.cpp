f310bc01476acf207e301082d0a812a82f06ac67
JHex <jhex125@gmail.com>
2023-07-19 16:50:28 +0800

    feat: check board

--- assignment6/src/materials/checker_board.cpp
+++ assignment6/src/materials/checker_board.cpp
@@ -1,16 +1,24 @@
 #include "materials/checker_board.h"
+#include "raytrace/hit.h"
 
 namespace materials
 {
 
 Vec3f CheckerBoard::Shade(const Ray &ray, const Hit &hit, const Vec3f &dirToLight, const Vec3f &lightColor) const
 {
-    assert(false);
-    return Vec3f(0, 0, 0);
+    Vec3f point = hit.getIntersectionPoint();
+    m_matrix->Transform(point);
+    bool u = ((int)std::floor(point.x()) % 2) == 0;
+    bool v = ((int)std::floor(point.y()) % 2) == 0;
+    bool w = ((int)std::floor(point.z()) % 2) == 0;
+    if (u ^ v ^ w)
+        return m_material1->Shade(ray, hit, dirToLight, lightColor);
+    else
+        return m_material2->Shade(ray, hit, dirToLight, lightColor);
 }
 
 void CheckerBoard::glSetMaterial() const
 {
-    assert(false);
+    m_material1->glSetMaterial();
 }
 } // namespace materials
